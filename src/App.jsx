0c181e5228456c8ebe1e45d72f02f3d5c2687779
enjoythedivision <marialapps@gmail.com>
2026-05-20 12:09:11 +0300

    update App component structure, remove unused styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,122 +1,80 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+
+const [course, setCourse] = useState({
+  title: "",
+  progress: 0,
+  difficulty: "Beginner",
+  notes: "",
+});
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+      <header>header</header>
+      <main>
+        <section>stats</section>
+        <section>
+          <form>
+            <div>
+              <label htmlFor="title">Course Title</label>
+              <input
+                type="text"
+                id="title"
+                placeholder="e.g. Advanced  React..."
+              />
+            </div>
+            <div>
+              <label htmlFor="progress">Progress</label>
+              <input type="range" id="progress" min={0} max={100} step={5} />
+            </div>
+            <div>
+              <label htmlFor="level">Difficulty</label>
+              <div>
+                <input
+                  type="radio"
+                  id="beginner"
+                  name="difficulty"
+                  value="Beginner"
+                />
 
-      <div className="ticks"></div>
+                <label htmlFor="beginner">Beginner</label>
+              </div>
+              <div>
+                <input
+                  type="radio"
+                  id="intermediate"
+                  name="difficulty"
+                  value="Intermediate"
+                />
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+                <label htmlFor="intermediate">Intermediate</label>
+              </div>
+              <div>
+                <input
+                  type="radio"
+                  id="advanced"
+                  name="difficulty"
+                  value="Advanced"
+                />
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+                <label htmlFor="advanced">Advanced</label>
+              </div>
+            </div>
+            <div>
+              <label htmlFor="notes">Notes</label>
+              <textarea name="notes" id="notes" placeholder="e.g. Need more practice with hooks..."></textarea>
+            </div>
+            <button type="submit">Add course</button>
+          </form>
+        </section>
+        <section>filters</section>
+        <section>course list</section>
+      </main>
+      <footer>footer</footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
