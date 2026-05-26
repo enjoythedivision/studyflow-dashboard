8edc1ae3ffb544569e2902252c908760e4eb16de
enjoythedivision <marialapps@gmail.com>
2026-05-26 12:47:56 +0300

    Implement login and signup forms with state management and API integration

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,21 +1,22 @@
+
 import { useState } from "react"
 
-function Login() {
-
+function Signup() {
     const [formData, setFormData] = useState({
         email: '', // required
-        password: '' // required
+        password: '', // required
+        username: '' // optional
     })
 
     function handleSubmit(e) {
         e.preventDefault()
-        fetch('http://localhost:3000/login', {
+        fetch('http://localhost:3000/users', {
             method: 'POST',
             headers: {'Content-Type' : 'application/json'},
             body: JSON.stringify(formData)
         })
         .then(res => res.json())
-        .then(data => console.log(data.user))
+        .then(data => console.log(data))
     }
 
     function handleChange(e) {
@@ -24,14 +25,15 @@ function Login() {
 
     return (
         <div>
-            <h1>Login Form</h1>
+            <h1>Signup Form</h1>
             <form className='login-form' onSubmit={e => handleSubmit(e)}>
+                <input type='text' placeholder='Username' value={formData.username} name='username' onChange={e => handleChange(e)} ></input>
                 <input type='text' placeholder='Email' value={formData.email} name='email' onChange={e => handleChange(e)} ></input>
                 <input type='text' placeholder='Password' value={formData.password} name='password' onChange={e => handleChange(e)} ></input>
-                <button className='login-btn' type='submit'>Login</button>
+                <button className='login-btn' type='submit'>Sign Up</button>
             </form>
         </div>
     )
 }
 
-export default Login
+export default Signup
