f7eb36c532073cd13622cd98521250763ca3c146
enjoythedivision <marialapps@gmail.com>
2026-09-02 17:35:52 +0300

    Implement CRUD methods in CoursesController

--- backend/Studyflow.Api/Controllers/CoursesController.cs
+++ backend/Studyflow.Api/Controllers/CoursesController.cs
@@ -4,5 +4,28 @@ using Microsoft.AspNetCore.Mvc;
 [Route("[controller]")]
 public class CoursesController : ControllerBase
 {
+    [HttpGet]
+    public IActionResult GetCourses()
+    {
+        
+    }
+
+    [HttpPost]
+    public IActionResult CreateCourses()
+    {
+        
+    }
+
+    [HttpPatch]
+    public IActionResult EditCourses()
+    {
+        
+    }
+
+    [HttpDelete]
+    public IActionResult DeleteCourses()
+    {
+        
+    }
     
 }
