8cc88476f740e42acbf95fbb0dcd2ff3c9fe87d2
enjoythedivision <marialapps@gmail.com>
2026-09-04 11:06:33 +0300

    Add JSON serialization options

--- backend/Studyflow.Api/Program.cs
+++ backend/Studyflow.Api/Program.cs
@@ -1,11 +1,19 @@
 using Microsoft.EntityFrameworkCore;
 using Studyflow.Api.Models;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services
+    .AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(
+            new JsonStringEnumConverter()
+        );
+    });
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
